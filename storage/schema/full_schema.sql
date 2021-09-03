f729ced378da301a295d62069374a33b06745eed
Usichenko Vlad <usichenko.vlad67@gmail.com>
2021-09-03 14:30:34 +0300

    add user_group and change category,course id to integer

--- storage/schema/full_schema.sql
+++ storage/schema/full_schema.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS categories (
-	"hash" varchar(255) NOT NULL,
+	"id" INTEGER NOT NULL,
 	"name" varchar(255) NOT NULL,
-	CONSTRAINT "category_pk" PRIMARY KEY ("hash")
+	CONSTRAINT "category_pk" PRIMARY KEY ("id")
 ) WITH (
   OIDS=FALSE
 );
@@ -9,13 +9,13 @@ CREATE TABLE IF NOT EXISTS categories (
 
 
 CREATE TABLE IF NOT EXISTS courses (
-	"hash" varchar(255) NOT NULL,
+	"id" INTEGER NOT NULL,
 	"name" varchar(255) NOT NULL,
-	"category" varchar(255) NOT NULL,
+	"category" INTEGER NOT NULL,
 	"trainer" varchar(255) NOT NULL,
 	"description" TEXT NOT NULL,
 	"price" integer NOT NULL,
-	CONSTRAINT "courses_pk" PRIMARY KEY ("hash")
+	CONSTRAINT "courses_pk" PRIMARY KEY ("id")
 ) WITH (
   OIDS=FALSE
 );
@@ -26,7 +26,7 @@ CREATE TABLE IF NOT EXISTS groups (
 	"id" serial NOT NULL,
 	"daytime" TEXT NOT NULL,
 	"type" BOOLEAN NOT NULL,
-	"course" varchar(255) NOT NULL,
+	"course" INTEGER NOT NULL,
 	"chat" varchar(255) NOT NULL,
 	CONSTRAINT "groups_pk" PRIMARY KEY ("id")
 ) WITH (
@@ -51,13 +51,26 @@ CREATE TABLE IF NOT EXISTS users (
 );
 
 
+CREATE TABLE user_group (
+	"user" integer NOT NULL,
+	"group" integer NOT NULL,
+	"type" varchar(255) NOT NULL
+) WITH (
+  OIDS=FALSE
+);
 
 
-ALTER TABLE "courses" ADD CONSTRAINT "courses_fk0" FOREIGN KEY ("category") REFERENCES "categories"("hash") ON UPDATE CASCADE ON DELETE CASCADE ;
 
-ALTER TABLE "groups" ADD CONSTRAINT "groups_fk0" FOREIGN KEY ("course") REFERENCES "courses"("hash") ON UPDATE CASCADE ON DELETE CASCADE ;
+
+ALTER TABLE "courses" ADD CONSTRAINT "courses_fk0" FOREIGN KEY ("category") REFERENCES "categories"("id") ON UPDATE CASCADE ON DELETE CASCADE ;
+
+ALTER TABLE "groups" ADD CONSTRAINT "groups_fk0" FOREIGN KEY ("course") REFERENCES "courses"("id") ON UPDATE CASCADE ON DELETE CASCADE ;
 
 ALTER TABLE "users" ADD CONSTRAINT "users_fk0" FOREIGN KEY ("id") REFERENCES "groups"("id") ON UPDATE CASCADE ON DELETE CASCADE ;
+
+ALTER TABLE "user_group" ADD CONSTRAINT "user_group_fk0" FOREIGN KEY ("user") REFERENCES "users"("id");
+
+ALTER TABLE "user_group" ADD CONSTRAINT "user_group_fk1" FOREIGN KEY ("group") REFERENCES "groups"("id");
 
 
 
